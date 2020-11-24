2bc8006e93d3485236df208fc7172939b5d56283
keiren <kzaracho@roshka.com>
2020-11-23 23:04:02 -0300

    initial

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Karen Zaracho on 11/22/20.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView()
+    }
+}
---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,20 @@
+//
+//  ratingView.swift
+//  Bookworm
+//
+//  Created by Karen Zaracho on 11/22/20.
+//
+
+import SwiftUI
+
+struct ratingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ratingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ratingView()
+    }
+}
